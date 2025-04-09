66b84f5bac664e6197b190460894ba7617a1a1e8
Nelly <kchen0116@gmail.com>
2025-04-09 13:28:20 +1000

    Add CRUD for todos

--- chapter-3/src/routes/todoRoutes.js
+++ chapter-3/src/routes/todoRoutes.js
@@ -10,15 +10,30 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+    const { task } = req.body
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+    
+    const result = insertTodo.run(req.userId, task)
 
+    res.json({ id: result.lastInsertRowid, task, completed: 0})
 })
 
 router.put('/:id', (req, res) => {
+    const { completed } = req.body
+    const { id } = req.params   // req.query e.g. ?page=2
 
+    const updataTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`) // For multiple values - SET task = ?, completed = ?
+    updateTodo.run(completed, id)
+
+    res.json({message: "Todo completed"})
 })
 
 router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    const { userId } = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
 
+    deleteTodo.run(id, userId)
 })
 
 export defaut router
